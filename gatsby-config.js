6b22c08d5d19ab1dfb1382720742787aa6c2df32
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2020-12-22 03:16:47 -0600

    Link icons to JSON links

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,32 @@ module.exports = {
     title: "Rustom Ichhaporia's Home on the Web",
     siteUrl: "https://rustom.dev",
     description: "The third iteration of Rustom Ichhaporia's personal website.",
-    socialLinks: {
-      email: "me@rustom.dev",
-      linkedin: "rustom-ichhaporia",
-      github: "rustom-ichhaporia", 
-      twitter: "cantspellrustom", 
-      instagram: "rustom.om", 
-      scholar: "fhRRnW8AAAAJ"
-    }
+    socialMedia: [
+      {
+        name: 'Email',
+        url: 'something at something'
+      },
+      {
+        name: 'GitHub',
+        url: 'https://github.com/rustom-ichhaporia',
+      },
+      {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/rustom-ichhaporia',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/rust.om',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/cantspellrustom',
+      },
+      {
+        name: 'Scholar',
+        url: 'https://scholar.google.com/citations?user=fhRRnW8AAAAJ'
+      }
+    ],
   },
   plugins: [
     {
@@ -19,14 +37,8 @@ module.exports = {
         trackingId: "UA-164610470-1",
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-styled-components", 
-    //   options: {
-
-    //   }
-    // },
     "gatsby-plugin-styled-components",
-    "styled-components", 
+    "styled-components",
     "babel-plugin-styled-components",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
