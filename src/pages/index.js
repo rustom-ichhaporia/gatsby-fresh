164c4348ec75483fd880a91c2681c5680563e4de
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2020-12-22 15:35:46 -0600

    Color icon links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,46 @@
 import React from "react"
 import styled from "styled-components"
 import GlobalStyle from '../globalStyles';
-// import { FaEnvelope, FaLinkedinIn, FaGithubAlt, FaInstagram, FaTwitter } from "@meronex/icons/fa";
-// import { SiGmail, SiGooglescholar } from "@meronex/icons/si";
 import Icon from '../icons'
 import { siteMetadata } from "../../gatsby-config";
 
 const Container = styled.div`
-  background-color: #04192e;
-  color: #ffffff;
+  background-color: var(--navy);
+  color: var(--white);
   height: 100vh;
   margin: 0px;
 
   align-items: center;
   justify-content: center;
   text-align: center;
-  font-family: IBM Plex Mono;
   font-size: 8vw;
   font-weight: 200;
   line-height: 10vw;
   letter-spacing: 0.2em;
-  // overflow: hidden;
+  overflow: hidden;
 `
 
-// const linkBox = styled.div`
-//   display: flex;
-//   background-color: "blue";
-// `;
+const linkBox = styled.div`
+  padding-left: 25px;
+`;
 
-// const button = styled.button`
-//   ::before {
-//     content: <SiGmail />
-//   }
-// `;
+export default function MainPage() {
 
-export default function UsersList() {
   return (
     <>
       <GlobalStyle />
       <Container>
+
         <p>RUSTOM<br />ICHHAPORIA</p>
-        {siteMetadata.socialMedia &&
-        siteMetadata.socialMedia.map(({ url, name }, i) => (
-          // <li key={i}>
-            <a href={url} aria-label={name}>
-              <Icon name={name} />
-            </a>
-          // </li>
-        ))}
-        {/* <linkBox>
-          <FaEnvelope />
-          <FaLinkedinIn />
-          <a href="https://linkedin.com/in/rustom-ichhaporia" target="_blank" rel="noreferrer"><FaGithubAlt aria-label="LinkedIn" style={{ color: "red", backgroundColor: "green" }} /></a>
-          <SiGooglescholar />
-          <FaTwitter />
-          <FaInstagram />
-        </linkBox> */}
+
+        <linkBox>
+          {siteMetadata.socialMedia &&
+            siteMetadata.socialMedia.map(({ url, name }, i) => (
+              <a href={url} aria-label={name} target="_blank" rel="noreferrer" style={{ paddingRight: '2vw' }}>
+                <Icon name={name} />
+              </a>
+            ))}
+        </linkBox>
 
       </Container>
     </>
