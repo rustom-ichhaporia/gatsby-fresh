abd969d9e26cdb99c7d6e8cab220c5973c72a902
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2020-12-26 14:15:20 -0600

    Add Prettier configuration

--- src/components/background.js
+++ src/components/background.js
@@ -1,42 +1,42 @@
-import React, { useRef, useState } from "react"
-import { Canvas, useFrame } from "react-three-fiber"
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 
-const Box = props => {
+const Box = (props) => {
   // This reference will give us direct access to the mesh so we can animate it
-  const mesh = useRef()
+  const mesh = useRef();
 
   // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
 
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
     <mesh
       {...props}
       ref={mesh}
       scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={e => setActive(!active)}
-      onPointerOver={e => setHover(true)}
-      onPointerOut={e => setHover(false)}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}
     >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial
         attach="material"
-        color={hovered ? "hotpink" : "orange"}
+        color={hovered ? 'hotpink' : 'orange'}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const IndexPage = () => (
-  <Canvas style={{position: "absolute", zIndex: 10}}>
+  <Canvas style={{ position: 'absolute', zIndex: 10 }}>
     <ambientLight />
     <pointLight position={[10, 10, 10]} />
     <Box position={[-1.2, 0, 0]} />
     <Box position={[1.2, 0, 0]} />
   </Canvas>
-)
+);
 
-export default IndexPage
+export default IndexPage;
--- src/components/icons.js
+++ src/components/icons.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { FaEnvelope, FaLinkedinIn, FaGithubAlt, FaInstagram, FaTwitter, FaMediumM } from "@meronex/icons/fa";
-import { SiGooglescholar } from "@meronex/icons/si";
+import {
+  FaEnvelope,
+  FaLinkedinIn,
+  FaGithubAlt,
+  FaInstagram,
+  FaTwitter,
+  FaMediumM,
+} from '@meronex/icons/fa';
+import { SiGooglescholar } from '@meronex/icons/si';
 
 const Icon = ({ name }) => {
-    switch (name) {
-        case 'Email':
-            return <FaEnvelope />;
-        case 'LinkedIn':
-            return <FaLinkedinIn />;
-        case 'GitHub':
-            return <FaGithubAlt />;
-        case 'Scholar':
-            return <SiGooglescholar />;
-        case 'Medium': 
-            return <FaMediumM />;
-        case 'Twitter':
-            return <FaTwitter />;
-        case 'Instagram':
-            return <FaInstagram />;
-        default: 
-            return <FaEnvelope />;
-    }
-}
+  switch (name) {
+    case 'Email':
+      return <FaEnvelope />;
+    case 'LinkedIn':
+      return <FaLinkedinIn />;
+    case 'GitHub':
+      return <FaGithubAlt />;
+    case 'Scholar':
+      return <SiGooglescholar />;
+    case 'Medium':
+      return <FaMediumM />;
+    case 'Twitter':
+      return <FaTwitter />;
+    case 'Instagram':
+      return <FaInstagram />;
+    default:
+      return <FaEnvelope />;
+  }
+};
 
 export default Icon;
