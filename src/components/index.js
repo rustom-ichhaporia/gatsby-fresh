ea8a7396716f510cf64c7c9573d8334c279c13ef
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-01-07 02:38:40 -0600

    Add components index export

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as About } from './about';
+export { default as Background } from './background';
+export { default as Cursor } from './cursor';
+export { default as Experience } from './experience';
+export { default as Home } from './home';
+export { default as Icons } from './icons';
+export { default as SocialLinks } from './social-links';
