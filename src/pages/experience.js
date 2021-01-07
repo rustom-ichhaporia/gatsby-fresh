f19abc470164adafc15ab580f288305df2067c1e
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-01-07 03:24:53 -0600

    Fix small scroll bar errors with hidden overflow css

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -2,6 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 import GlobalStyle from '@styles/global-style';
-import Experience from '@components/experience';
-import { Cursor, Experience, Icons, SocialLinks} from '@components';
+import { Cursor, Experience, SocialLinks } from '@components';
 
+const PageStyle = styled.div`
+  max-width: 700px;
+  margin: auto;
+  padding: 20px;
+
+  font-size: 30px;
+  line-height: 1.3em;
+`;
+
+export default function Page() {
+  return (
+    <>
+      <GlobalStyle />
+      <Experience />
+      <SocialLinks />
+      <Cursor />
+    </>
+  );
+}
